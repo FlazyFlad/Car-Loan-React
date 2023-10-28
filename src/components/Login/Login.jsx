ff4ec4a64511e51fcda2287aab5186250a8238df
Isaac177 <isaacjogues177@gmail.com>
2023-10-28 16:27:50 +0600

    Unstaged changes

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,14 +5,13 @@ import { Link } from "react-router-dom";
 const Login = () => {
     return (
         <>
-        
-        <div class="login-container">
-            <div class="form-container">
+        <div className="login-container">
+            <div className="form-container">
                 <h2>Login to Your Account</h2>
-                <form id="login-form" class="form">
+                <form id="login-form" className="form">
                     <input type="email" placeholder="Email" required />
                     <input type="password" placeholder="Password" required />
-                    <button type="submit" class="cta-button">Login</button>
+                    <button type="submit" className="cta-button">Login</button>
                     <p>Don't have an account?
                         <Link style={{marginLeft: '5px'}} to="/register">
                             Register
@@ -21,7 +20,6 @@ const Login = () => {
                 </form>
             </div>
         </div>
-
         </>
     );
 }
